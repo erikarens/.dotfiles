94f90dec93027c7938ffcd372d195bdf1203ebae
Erik Arens <erikarens@netzhero.de>
2024-08-02 07:54:04 +0200

    Updated which-key config

--- .config/nvim/lua/netzhero/plugins/which-key.lua
+++ .config/nvim/lua/netzhero/plugins/which-key.lua
@@ -1,23 +1,21 @@
 -- Useful plugin to show you pending keybinds.
 return {
-"folke/which-key.nvim",
-event = "VimEnter", -- Sets the loading event to 'VimEnter'
-config = function() -- This is the function that runs, AFTER loading
+  "folke/which-key.nvim",
+  event = "VimEnter", -- Sets the loading event to 'VimEnter'
+  config = function() -- This is the function that runs, AFTER loading
     require("which-key").setup()
 
     -- Document existing key chains
-    require("which-key").register({
-        ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-        ["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-        ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-        ["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-        ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-        ["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
-        ["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
+    require("which-key").add({
+      { "<leader>c", "which_key_ignore", desc = "[C]ode" },
+      { "<leader>d", "which_key_ignore", desc = "[D]ocument" },
+      { "<leader>r", "which_key_ignore", desc = "[R]ename" },
+      { "<leader>s", "which_key_ignore", desc = "[S]earch" },
+      { "<leader>w", "which_key_ignore", desc = "[W]orkspace" },
+      { "<leader>t", "which_key_ignore", desc = "[T]oggle" },
+      { "<leader>h", "which_key_ignore", desc = "Git [H]unk" },
+      -- visual mode
+      { "<leader>h", desc = "Git [H]unk", mode = "v" },
     })
-    -- visual mode
-    require("which-key").register({
-        ["<leader>h"] = { "Git [H]unk" },
-    }, { mode = "v" })
-end,
+  end,
 }
