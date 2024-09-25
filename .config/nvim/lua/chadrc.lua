77155572235bd69790b8cdd626a5f6475f4872e0
Erik Arens <erikarens@netzhero.de>
2024-09-25 08:35:14 +0200

    Updates nvim config

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -1,17 +1,52 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
-M.base46 = {
-	theme = "github_dark",
+-- disable tabufline, i want to use bufferline
+M.ui = {
+  tabufline = {
+    enabled = false,
+    lazyload = true,
+    order = { "treeOffset", "buffers", "tabs", "btns" },
+    modules = nil,
+  },
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  -- enables dashboard on startup and adjusts design
+  nvdash = {
+    load_on_startup = false,
+
+    header = {
+      "                                                     ",
+      "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+      "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+      "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+      "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+      "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+      "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+      "                                                     ",
+    },
+
+    buttons = {
+      { "  Find File", "Spc f f", "Telescope find_files" },
+      { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+      { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+      { "  Bookmarks", "Spc m a", "Telescope marks" },
+      { "  Themes", "Spc t h", "Telescope themes" },
+      { "  Mappings", "Spc c h", "NvCheatsheet" },
+    },
+  },
+}
+
+M.base46 = {
+  theme = "github_dark",
+
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
 }
 
 return M
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,7 +1,7 @@
 return {
   {
     "stevearc/conform.nvim",
-    event = 'BufWritePre', -- uncomment for format on save
+    event = "BufWritePre", -- uncomment for format on save
     opts = require "configs.conform",
   },
 
@@ -22,6 +22,53 @@ return {
     config = function()
       -- Optional additional config
     end,
+  },
+
+  -- Bufferline plugin
+  {
+    "akinsho/bufferline.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" }, -- For icons
+    version = "*", -- Use latest version
+    lazy = false, -- Ensure Bufferline is loaded at startup
+    opts = {
+      options = {
+        mode = "tabs", -- Only show tabs (no buffers)
+        separator_style = "slant", -- Choose separator style
+        show_buffer_close_icons = false, -- Hide close icons on buffers
+        show_close_icon = false, -- Hide global close icon
+        diagnostics = "nvim_lsp", -- Show LSP diagnostics
+        offsets = {
+          { filetype = "NvimTree", text = "File Explorer", highlight = "Directory", text_align = "left" },
+        },
+      },
+    },
+    config = function(_, opts)
+      require("bufferline").setup(opts)
+    end,
+  },
+
+  -- Auto-session plugin
+  {
+    "rmagatti/auto-session",
+    config = function()
+      local auto_session = require "auto-session"
+      auto_session.setup {
+        auto_restore_enabled = false,
+        auto_session_suppress_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
+      }
+    end,
+  },
+
+  -- Git blame plugin
+  {
+    "f-person/git-blame.nvim",
+    event = "VeryLazy", -- Load the plugin lazily
+    opts = {
+      enabled = true, -- Enable the plugin
+      message_template = " <author> • <date> • <<sha>>", -- Customize the git blame message
+      date_format = "%m-%d-%Y", -- Customize the date format
+      virtual_text_column = 1, -- Set where the virtual text should appear
+    },
   },
 
   -- These are some examples, uncomment them if you want to see them work!
