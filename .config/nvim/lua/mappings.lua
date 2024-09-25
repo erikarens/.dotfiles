d8c2dc735051b98714dc320de6fedc6b735db660
Erik Arens <erikarens@netzhero.de>
2024-09-25 08:57:39 +0200

    Added telescope keymaps

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -45,6 +45,20 @@ map("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explor
 map("n", "<leader>.r", "<cmd>SessionRestore<CR>", { desc = "Restore session" })
 map("n", "<leader>.s", "<cmd>SessionSave<CR>", { desc = "Save session" })
 
+-- Telescope
+local builtin = require "telescope.builtin"
+map("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+map("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+map("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+map("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+map("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+map("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+map("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+map("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
+map("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+map("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+map("n", "<leader><ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+
 -- Uncomment if you need save with Ctrl-S
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd>w<cr>", { desc = "Save file" })
 
